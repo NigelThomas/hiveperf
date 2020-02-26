7c1b29bd1fbc0cf2f57ddcccf64d254255d27036
NigelThomas <nigel.cl.thomas@gmail.com>
2020-02-26 11:40:33 +0000

    clean up startup

--- hiveperf.sh
+++ hiveperf.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #
 # start a development container, load all slab files from the current project
-# expects BASE_IMAGE and CONTAINER_NAME to be supplied by caller 
+
+HERE=$(cd `dirname $0`; pwd)
+BASE_IMAGE=sqlstream/streamlab-git
+CONTAINER_NAME=hiveperf
+
+# This is a SQL project - there are no SLAB files
+: ${LOAD_SLAB_FILES:=}
 
 GIT_ACCOUNT=https://github.com/NigelThomas
 GIT_PROJECT_NAME=hiveperf
+
+# default image label is latest 6.0.1 (:release)
 : ${BASE_IMAGE_LABEL:=release}
 
 docker kill $CONTAINER_NAME
@@ -18,12 +26,11 @@ then
     HOST_JNDI_MOUNT="-v ${HOST_JNDI_DIR:=$HERE/jndi}:$CONTAINER_JNDI_DIR"
 fi
 
-# set mount for credentials
-: ${HOST_CRED_MOUNT:=$HOME/credentials}
+# override default host mount point for credentials using HOST_CRED_MOUNT
 
 docker run $HOST_JNDI_MOUNT \
-           -v $HOME/credentials:/home/sqlstream/credentials \
-           -p 80:80 -p 5560:5560 -p 5580:5580 -p 5585:5585 -p 5590:5590 \
+           -v ${HOST_CRED_MOUNT:=$HOME/credentials}:/home/sqlstream/credentials \
+           -p 5570:5570 -p 5580:5580 \
            -e GIT_ACCOUNT=$GIT_ACCOUNT -e GIT_PROJECT_NAME=$GIT_PROJECT_NAME -e GIT_PROJECT_HASH=$GIT_PROJECT_HASH \
            -e LOAD_SLAB_FILES="${LOAD_SLAB_FILES:=}" \
            -e SQLSTREAM_HEAP_MEMORY=${SQLSTREAM_HEAP_MEMORY:=4096m} \
