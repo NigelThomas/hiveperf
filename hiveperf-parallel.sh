56986296eae03362382344724948593e0a71164c
NigelThomas <nigel.cl.thomas@gmail.com>
2020-02-28 11:58:45 +0000

    extract logs from containers

--- hiveperf-parallel.sh
+++ hiveperf-parallel.sh
@@ -1,6 +1,5 @@
-tdir=test-`date +"%Y%m%d%H%M"`
+tdir=test-`date +"%Y.%m.%d-%H.%M"`
 mkdir $tdir
-cd $tdir
 
 for p in `seq -w 1 $1`
 do
@@ -8,10 +7,24 @@ do
     echo "starting $CONTAINER_NAME"
     ./hiveperf.sh
 done
+cd $tdir
 
-echo "hiveperf-parallel with $1 containers started at `date` logging to $tdir"
+echo "hiveperf-parallel with $1 containers started at `date` logging to $tdir" > summary.txt
 
 # stats every 15 secs for 20 minutes with timestamp and wide format
-vmstat -t -w 15 80 | tee vmstat.logging
+vmstat -t -w 15 80 | tee vmstat.log
+
+for p in `seq -w 1 $1`
+do
+    export CONTAINER_NAME=hiveperf$p
+    # collect trace log and stream stats
+    docker cp ${CONTAINER_NAME}:/var/log/sqlstream/Trace.log.0 ${CONTAINER_NAME}.trace.log
+    docker cp ${CONTAINER_NAME}:/home/sqlstream/monitor/edr_minute_count-${CONTAINER_NAME}.csv .
+done
+
+cd ..
+pwd
+
+echo Test $tdir completed at `date` >>summary.txt
 
 
